de00eeac3e77e13444ccb26891c256c517e528cf
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-10-30 20:19:33 +0530

    render server response

--- static/index.js
+++ static/index.js
@@ -19,6 +19,34 @@ $('#doc-form').submit(function(event) {
 });
 
 function updatePredictions(predictions) {
-    $('#doc1-pred').text(predictions.doc1.label)
-    $('#doc2-pred').text(predictions.doc2.label)
+    $('#doc1-pred').html(probability_table(predictions.doc1.prob));
+    $('#doc2-pred').html(probability_table(predictions.doc2.prob));
+    $('#doc1-label').html(label(predictions.doc1.label));
+    $('#doc2-label').html(label(predictions.doc2.label));
+}
+
+function probability_table(probs) {
+    var table = `
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Label</th>
+                    <th scope="col">Probability</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${probs.map(prob => `
+                    <tr>
+                        <th scope="row">${prob[0]}</th>
+                        <td>${prob[1]}</td>
+                    </tr>
+                `).join('')}
+            </tbody>
+        </table>
+    `;
+    return table;
+}
+
+function label(label) {
+    return `<h1 class="text-center">${label}</h1>`;
 }
