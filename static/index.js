771804c86546449719a158d3ba44baae09376190
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-10-30 19:42:00 +0530

    client make POST /predict

--- static/index.js
+++ static/index.js
@@ -1,1 +1,24 @@
-console.log('heheheheh')
+$('#doc-form').submit(function(event) {
+    var doc1 = $('textarea#doc1').val();
+    var doc2 = $('textarea#doc2').val();
+    var data = {
+        doc1: doc1,
+        doc2: doc2
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/predict',
+        data: JSON.stringify(data),
+        success: function(response) {
+            updatePredictions(response);
+        },
+        contentType: 'application/json'
+    });
+    event.preventDefault();
+});
+
+function updatePredictions(predictions) {
+    $('#doc1-pred').text(predictions.doc1.label)
+    $('#doc2-pred').text(predictions.doc2.label)
+}
