1f4a6873b09d13ab7a8113dd857c6e61a234184c
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-10-30 18:35:45 +0530

    client libs setup; predictions function

---
+++ predict.py
@@ -0,0 +1,49 @@
+import pickle
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+DATA_DIR = "./bbc/"
+data = load_files(DATA_DIR, encoding="utf-8", decode_error="replace")
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target)
+
+with open('model.pickle', 'rb') as fd:
+    model = pickle.load(fd)
+with open('vectorizer.pickle', 'rb') as fd:
+    vectorizer = pickle.load(fd)
+
+f1 = open("input1.txt","r")
+f2 = open("input2.txt","r")
+inputs = [f1.read(),f2.read()]
+
+# Testing
+# y_pred1 = model.predict(vectorizer.transform(X_test))
+# print("Model's Accuracy is " + str(100*accuracy_score(y_test,y_pred1)) + "%")
+
+def predictions(docs=[open('input1.txt', 'r').read(), open('input2.txt', 'r').read()]):
+    docs_trans = vectorizer.transform(docs)
+    pred = model.predict(docs_trans)
+    pred_proba = model.predict_proba(docs_trans)
+
+    concept_diff = 0.0
+    num_labels = len(data.target_names)
+    for i in range(num_labels):
+        diff = abs(pred_proba[0][i] - pred_proba[1][i])
+        concept_diff = max(concept_diff, diff)
+    
+    probs1, probs2 = pred_proba.tolist()
+    probs1 = [(data.target_names[i], prob) for i, prob in enumerate(probs1)]
+    probs2 = [(data.target_names[i], prob) for i, prob in enumerate(probs2)]
+    result = {
+        'concept_diff': concept_diff,
+        'doc1': {
+            'prob': probs1,
+            'label': data.target_names[pred[0]]
+        },
+        'doc2': {
+            'prob': probs2,
+            'label': data.target_names[pred[1]]
+        }
+    }
+
+    return result
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import pickle
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
@@ -29,24 +30,8 @@ vectorizer.fit(X_train)
 model = MultinomialNB()
 # transform the list of text to tf-idf before passing it to the model
 model.fit(vectorizer.transform(X_train), y_train)
- 
-f1 = open("input1.txt","r")
-f2 = open("input2.txt","r")
-inputs = [f1.read(),f2.read()]
 
-# Testing
-y_pred1 = model.predict(vectorizer.transform(X_test))
-print("Model's Accuracy is " + str(100*accuracy_score(y_test,y_pred1)) + "%")
-
-y_pred = model.predict_proba(vectorizer.transform(inputs))
-yy = model.predict(vectorizer.transform(inputs))
-print("The first document mainly talks about --> "+data.target_names[yy[0]])
-print("The second document mainly talks about -->"+data.target_names[yy[1]])
-
-
-concept_diff = 0.0
-
-for i in range(len(y_pred[0])):
-    concept_diff = max(concept_diff,(abs(y_pred[0][i]-y_pred[1][i])))
-print("Concept difference in given documents (%) is: ")
-print(str(100*(concept_diff))+" %")
+with open('model.pickle', 'wb') as fd:
+    pickle.dump(model, fd)
+with open('vectorizer.pickle', 'wb') as fd:
+    pickle.dump(vectorizer, fd)
