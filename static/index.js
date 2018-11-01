c72c1ca58c3e683d8714ed719ba4bb5a17b41eaf
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-11-01 18:47:33 +0530

    extract predictionCols component; render concept drift

--- static/index.js
+++ static/index.js
@@ -19,11 +19,18 @@ $('#doc-form').submit(function(event) {
 });
 
 function updatePredictions(predictions) {
-    var predictions = `
-        ${predictionRow('Probability', probabilityTable(predictions.doc1.prob), probabilityTable(predictions.doc2.prob))}
-        ${predictionRow('Labels', label(predictions.doc1.label), label(predictions.doc2.label))}
-    `;
-    $('#prediction-container').html(predictions);
+    var prob1 = predictions.doc1.prob;
+    var prob2 = predictions.doc2.prob;
+    var label1 = predictions.doc1.label;
+    var label2 = predictions.doc2.label;
+    var concept_diff = predictions.concept_diff;
+
+    var result = '';
+    result += predictionRow('Probability', predictionCols(probabilityTable(prob1), probabilityTable(prob2)));
+    result += predictionRow('Labels', predictionCols(label(label1), label(label2)));
+    result += predictionRow('Concept Difference', conceptDiff(concept_diff));
+
+    $('#prediction-container').html(result);
 }
 
 function probabilityTable(probs) {
@@ -39,7 +46,7 @@ function probabilityTable(probs) {
                 ${probs.map(prob => `
                     <tr>
                         <th scope="row">${prob[0]}</th>
-                        <td>${prob[1].toFixed(7)}</td>
+                        <td>${prob[1].toFixed(5)}</td>
                     </tr>
                 `).join('')}
             </tbody>
@@ -49,13 +56,13 @@ function probabilityTable(probs) {
 }
 
 function label(label) {
-    var label = `
+    var result = `
         <h3 class="pred-label">${label}</h3>
     `;
-    return label;
+    return result;
 }
 
-function predictionRow(title, doc1, doc2) {
+function predictionRow(title, body) {
     var row = `
         <div class="row">
             <div class="col">
@@ -63,11 +70,28 @@ function predictionRow(title, doc1, doc2) {
                     <div class="col"><h2>${title}</h2></div>
                 </div>
                 <div class="row">
-                    <div class="col">${doc1}</div>
-                    <div class="col">${doc2}</div>
+                    ${body}
                 </div>
             </div>
         </div>
     `;
     return row;
+}
+
+function predictionCols(col1, col2) {
+    var result = `
+        <div class="col">${col1}</div>
+        <div class="col">${col2}</div>
+    `;
+    return result;
+}
+
+function conceptDiff(num) {
+    var percentage = (num * 100).toFixed(2);
+    var result = `
+        <div class="col">
+            <h3 id="concept-diff">${percentage}%</h3>
+        </div>
+    `;
+    return result;
 }
