1b17f1ac428fcfd330fa9a0b6bed8f649d558bb8
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-11-01 18:58:23 +0530

    page heading desc; change js to es6

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 $('#doc-form').submit(function(event) {
-    var doc1 = $('textarea#doc1').val();
-    var doc2 = $('textarea#doc2').val();
-    var data = {
+    const doc1 = $('textarea#doc1').val();
+    const doc2 = $('textarea#doc2').val();
+    const data = {
         doc1: doc1,
         doc2: doc2
     };
@@ -19,13 +19,11 @@ $('#doc-form').submit(function(event) {
 });
 
 function updatePredictions(predictions) {
-    var prob1 = predictions.doc1.prob;
-    var prob2 = predictions.doc2.prob;
-    var label1 = predictions.doc1.label;
-    var label2 = predictions.doc2.label;
-    var concept_diff = predictions.concept_diff;
+    const { prob: prob1, label: label1 } = predictions.doc1;
+    const { prob: prob2, label: label2 } = predictions.doc2;
+    const { concept_diff } = predictions;
 
-    var result = '';
+    let result = '';
     result += predictionRow('Probability', predictionCols(probabilityTable(prob1), probabilityTable(prob2)));
     result += predictionRow('Labels', predictionCols(label(label1), label(label2)));
     result += predictionRow('Concept Difference', conceptDiff(concept_diff));
@@ -34,7 +32,7 @@ function updatePredictions(predictions) {
 }
 
 function probabilityTable(probs) {
-    var table = `
+    const table = `
         <table class="table">
             <thead class="thead-light">
                 <tr>
@@ -56,14 +54,14 @@ function probabilityTable(probs) {
 }
 
 function label(label) {
-    var result = `
+    const result = `
         <h3 class="pred-label">${label}</h3>
     `;
     return result;
 }
 
 function predictionRow(title, body) {
-    var row = `
+    const row = `
         <div class="row">
             <div class="col">
                 <div class="row pred-title">
@@ -79,7 +77,7 @@ function predictionRow(title, body) {
 }
 
 function predictionCols(col1, col2) {
-    var result = `
+    const result = `
         <div class="col">${col1}</div>
         <div class="col">${col2}</div>
     `;
@@ -87,8 +85,8 @@ function predictionCols(col1, col2) {
 }
 
 function conceptDiff(num) {
-    var percentage = (num * 100).toFixed(2);
-    var result = `
+    const percentage = (num * 100).toFixed(2);
+    const result = `
         <div class="col">
             <h3 id="concept-diff">${percentage}%</h3>
         </div>
