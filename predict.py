1fb551780146eb0f6699d588a338634d508805ee
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-12-02 18:38:44 +0530

    change concept_diff algo to be the length of delta

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import pickle
+from math import sqrt
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -19,17 +20,14 @@ def predictions(docs):
     pred = model.predict(docs_trans)
     pred_proba = model.predict_proba(docs_trans)
 
-    concept_diff = 0.0
-    num_labels = len(data.target_names)
-    for i in range(num_labels):
-        diff = abs(pred_proba[0][i] - pred_proba[1][i])
-        concept_diff = max(concept_diff, diff)
-    
     probs1, probs2 = pred_proba.tolist()
+
+    diff = concept_diff(tuple(probs1), tuple(probs2))
     probs1 = [(data.target_names[i], prob) for i, prob in enumerate(probs1)]
     probs2 = [(data.target_names[i], prob) for i, prob in enumerate(probs2)]
+
     result = {
-        'concept_diff': concept_diff,
+        'concept_diff': diff,
         'doc1': {
             'prob': probs1,
             'label': data.target_names[pred[0]]
@@ -41,3 +39,13 @@ def predictions(docs):
     }
 
     return result
+
+def concept_diff(probs1, probs2):
+    mag_max = sqrt(2)
+    return mag(delta(probs1, probs2)) / mag_max
+
+def mag(point):
+    return sqrt(sum([x**2 for x in point]))
+
+def delta(p1, p2):
+    return tuple([x1 - x2 for (x1, x2) in zip(p1, p2)])
